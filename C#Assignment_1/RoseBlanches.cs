1f31b457211168d8d39378390bd267b1e173a4e3
samr222 <samragyeemalla22@gmail.com>
2024-02-12 10:18:57 -0500

    first commit

---
+++ C#Assignment_1/RoseBlanches.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sam
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How much did you receive money ($)? ");
+            double totalmoney= Convert.ToDouble(Console.ReadLine());
+
+            double books = totalmoney* 0.75;
+            double restMoney = totalmoney - books;
+            double MoneyperItem = restMoney / 3;
+            double coffee = (MoneyperItem / 2);//dividing by 2 as the coffee costs 2 dollars
+            double change1 = (MoneyperItem % 2);//collecting change for the white roses
+            double computer = (MoneyperItem / 4);// cost of computer is 4 dollars
+            double change2 = (MoneyperItem % 4);
+            double subway = (MoneyperItem/ 3);//cost of tickets is 3 dollars
+            double change3 = (MoneyperItem % 3);
+
+            Console.WriteLine("Book and Supplies: " + books + " $");
+
+            Console.WriteLine("You can then buy: ");
+
+            Console.WriteLine(Math.Floor(coffee) + " coffees");
+
+            Console.WriteLine(Math.Floor(computer) + " Flash Computer Numbers");
+
+            Console.WriteLine(Math.Floor(subway) + " Subway Tickets");
+
+            Console.WriteLine("and you will have " + Math.Floor(change1 + change2 + change3) + " dollars for the white roses.");
+
+            Console.ReadKey();
+
+
+                
+        }
+    }
+
+}
+
+
---
+++ C#Assignment_2/Interval.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ask the user to enter a real number
+        Console.Write("Enter a real number: ");
+        double x = double.Parse(Console.ReadLine());
+
+        // Define the intervals
+        double interval1Start = 2;
+        double interval1End = 3;
+        double interval2Start = 0;
+        double interval2End = 1;
+        double interval3Start = -10;
+        double interval3End = -2;
+
+        // Test membership using only < and ==
+        bool condition1 = (x >= interval1Start) && (x < interval1End);
+        bool condition2 = (x > interval2Start) && (x <= interval2End);
+        bool condition3 = (x >= interval3Start) && (x <= interval3End);
+
+        // Display the result based on logical operators
+        if (((condition1 || condition2) || (condition3)))
+        {
+            Console.WriteLine("x belongs to I");
+        }
+        else
+        {
+            Console.WriteLine("x does not belong to I");
+        }
+    }
+}
